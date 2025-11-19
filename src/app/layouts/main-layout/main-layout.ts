0120e082d094953cebd40c337ef696161fe7ae9b
igor <igor.chernyshov6636@gmail.com>
2025-11-19 16:21:29 +1100

    Fix Angular warnings: remove optional chaining and unused imports, remove _redirects

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
