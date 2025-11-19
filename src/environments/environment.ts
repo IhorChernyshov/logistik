f8df9067aa8f796b83a45cc056334f0608474c39
igor <igor.chernyshov6636@gmail.com>
2025-11-20 01:55:44 +1100

    Improved styles and image - F

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // Production environment configuration
 export const environment = {
   production: true,
-  apiUrl: 'web-production-b40e.up.railway.app' // Railway backend with CORS
+  apiUrl: 'web-production-dbe6.up.railway.app' // Railway backend with CORS
 };
